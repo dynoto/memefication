53ed365780521f7ed61038c93a95305cbdea558e
David Tjokroaminoto <dynamic.dante@gmail.com>
2015-08-20 21:48:05 +0800

    fix inconsistent uitextfield size add test devices so admob doesn't display ads on test phones

--- memefication/MemeShareViewController.m
+++ memefication/MemeShareViewController.m
@@ -76,8 +76,8 @@
     self.bannerView.adUnitID = @"ca-app-pub-1696149948739760/6150731937";
     self.bannerView.rootViewController = self;
     GADRequest *request = [GADRequest request];
-    request.testDevices = @[ @"7434b48a0e959dfb308923c72c435c42" ];
-    [self.bannerView loadRequest:[GADRequest request]];
+    request.testDevices = @[ @"7434b48a0e959dfb308923c72c435c42", kGADSimulatorID ];
+    [self.bannerView loadRequest:request];
 }
 
 
